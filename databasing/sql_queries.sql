7ca11a3d705e7d823b02121e830ca6688819bebd
Raphael Hetherington <raphael.hetherington@ucdconnect.ie>
2019-02-20 13:53:50 +0000

    changed sql queries

--- databasing/sql_queries.sql
+++ databasing/sql_queries.sql
@@ -12,4 +12,50 @@ FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
 
--- test
+-- CREATE STATIC DATA
+
+CREATE TABLE staticdata (
+number INT PRIMARY KEY, 
+name VARCHAR(255) NOT NULL, 
+address VARCHAR(255) NOT NULL, 
+latitude DECIMAL(10,8) NOT NULL, 
+longitude DECIMAL(11,8) NOT NULL);
+
+-- CREATE WEATHER DATA
+
+CREATE TABLE weatherdata (
+id INT PRIMARY KEY,
+weather_text VARCHAR(255),
+temperature DECIMAL(3,1)
+)
+
+-- CREATE DYNAMIC DATA
+
+CREATE TABLE dynamicdata (
+station_id INT,
+timestamp INT(15),
+weather_id INT,
+banking VARCHAR(10),
+bonus VARCHAR(10),
+total_bike_stands INT,
+available_bike_stands INT,
+available_bikes INT,
+status VARCHAR(10),
+weekday VARCHAR(20),
+hour_and_minute TIME,
+
+CONSTRAINT pk_station PRIMARY KEY (station_id, timestamp),
+
+CONSTRAINT station_fk
+FOREIGN KEY (station_id)
+REFERENCES staticdata (number)
+ON DELETE RESTRICT
+ON UPDATE CASCADE,
+
+CONSTRAINT weather_fk
+FOREIGN KEY (weather_id)
+REFERENCES weatherdata (id)
+ON DELETE RESTRICT
+ON UPDATE CASCADE
+
+);
