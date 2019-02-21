4e929aab193c14d0fa6149dd012f49de666e6966
kierandaly88 <46922666+kierandaly88@users.noreply.github.com>
2019-02-21 11:41:25 +0000

    Delete private.py
    Private file pushed to GitHub by accident

--- private.py
+++
@@ -1,7 +0,0 @@
-api="2029934b383854098ac75c16c62e5f48763e5934"
-stationsURI="https://api.jcdecaux.com/vls/v1/stations"
-
-currentUser='root'
-mypassword='kd1988kd'
-myhost='localhost'
-myDB='myTestDB'
