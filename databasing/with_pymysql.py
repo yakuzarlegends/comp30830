9e7458b0442f602553e725969b5f769ba3500151
Raphael Hetherington <raphael.hetherington@ucdconnect.ie>
2019-03-07 11:35:03 +0000

    adjusted with Yvette's scraper

--- databasing/with_pymysql.py
+++ databasing/with_pymysql.py
@@ -3,25 +3,63 @@ import time
 import csv
 import json
 import calendar
-import private
 import pymysql
 from datetime import datetime
+import os
 
 class Scraper:
-    
-    WEATHER_APIKEY = private.yvette_weather_api
-    BIKE_APIKEY = private.jcd_api
+
+    DETAILS = "true"    
+    METRIC = "true" 
+    NAME="dublin"
     LANGUAGE = "en-us"
-    DETAILS = "false"
-    URL = private.weather_url
-    STATIONS_URL = private.jcd_url
-    WEATHER_PATH = private.weather
-    STATIONS_PATH = private.stations
-    NAME = "dublin"
-    STATIC_PATH = private.static_path
-    SQL_USER = private.raph_rds_user
-    SQL_HOST = private.raph_rds_host
-    PASSWORD = private.raph_rds_pw
+
+    STATIC_PATH = "./static.csv"
+
+    # WEATHER
+    YVETTE_WEATHER_API = os.environ['YVETTE_WEATHER_API']
+    RAPH_WEATHER_API = os.environ['RAPH_WEATHER_API']
+    WEATHER_URL = os.environ['WEATHER_URL']
+
+    # BIKES
+
+    JCD_API = os.environ['JCD_API']
+    JCD_URL = os.environ['JCD_URL']
+
+    # Kieran RDS
+
+    KIERAN_RDS_HOST = os.environ['KIERAN_RDS_HOST']
+    KIERAN_RDS_USER = os.environ['KIERAN_RDS_USER']
+    KIERAN_RDS_PW = os.environ['KIERAN_RDS_PW']
+
+    # Raph RDS
+
+    RAPH_RDS_PW = os.environ['RAPH_RDS_PW']
+    RAPH_RDS_USER = os.environ['RAPH_RDS_USER']
+    RAPH_RDS_HOST = os.environ['RAPH_RDS_HOST']
+
+    # CSVs
+
+    STATIONS = os.environ['STATIONS']
+    WEATHER = os.environ['WEATHER']
+
+
+
+
+    
+    # WEATHER_APIKEY = private.yvette_weather_api
+    # BIKE_APIKEY = private.jcd_api
+ 
+    # DETAILS = "false"
+    # URL = private.weather_url
+    # STATIONS_URL = private.jcd_url
+    # WEATHER_PATH = private.weather
+    # STATIONS_PATH = private.stations
+    # NAME = "dublin"
+    # STATIC_PATH = private.static_path
+    # SQL_USER = private.raph_rds_user
+    # SQL_HOST = private.raph_rds_host
+    # PASSWORD = private.raph_rds_pw
     # SQL_USER = private.sql_user
     # SQL_HOST = private.sql_host
     # PASSWORD = private.pw
@@ -29,7 +67,7 @@ class Scraper:
 
     def __init__(self):
         self._jcd_counter = 1
-        self._weather_counter = 0
+        self._weather_counter = 1
         self._bike_json = []
         self.weather_json = []
 
@@ -42,16 +80,18 @@ class Scraper:
             if self._jcd_counter == self._weather_counter * 12 + 1:
                 self._weather_counter += 1
                 self.populate_weather()
-        self.populate_bikes()   
-        
+            self.populate_bikes()     
+
+   
     def build_static_data(self):
         self.insert_to_db("staticdata", Scraper.STATIC_PATH)
+
 
     def populate_weather(self):
         print("WEATHER COUNTER AT START: ", self._weather_counter)
         data = self.get_weather_json()
         self.build_weather_csv(data, self._weather_counter)
-        self.insert_to_db("weatherdata", Scraper.WEATHER_PATH)
+        self.insert_to_db("weatherdata", Scraper.WEATHER)
         print("Weather successfully inserted")
         print("WEATHER COUNTER AT END: ", self._weather_counter)
         
@@ -60,7 +100,7 @@ class Scraper:
         data = self.get_bike_json()
         print(self._weather_counter, self._jcd_counter, "here!!")
         self.build_bike_csv(data, self._weather_counter, self._jcd_counter)
-        self.insert_to_db("dynamicdata", Scraper.STATIONS_PATH)
+        self.insert_to_db("dynamicdata", Scraper.STATIONS)
         print("successfully populated bikes")
         self._jcd_counter += 1
         time.sleep(5*60)
@@ -68,7 +108,7 @@ class Scraper:
     def get_bike_json(self):
         while True:
             try: 
-                r = requests.get(Scraper.STATIONS_URL, params={"apiKey": Scraper.BIKE_APIKEY,"contract": Scraper.NAME})
+                r = requests.get(Scraper.JCD_URL, params={"apiKey": Scraper.JCD_API,"contract": Scraper.NAME})
                 print("bikes received")
                 self._bike_json = json.loads(r.text)
                 return self._bike_json
@@ -79,7 +119,7 @@ class Scraper:
     def get_weather_json(self):
         while True:
             try:
-                r = requests.get(Scraper.URL, params={"apikey": Scraper.WEATHER_APIKEY, "language": Scraper.LANGUAGE,"details": Scraper.DETAILS})
+                r = requests.get(Scraper.WEATHER_URL, params={"apikey": Scraper.YVETTE_WEATHER_API, "language": Scraper.LANGUAGE,"details": Scraper.DETAILS,"metric": Scraper.METRIC})
                 print("weather received")
                 self.weather_json = json.loads(r.text)
                 return self.weather_json
@@ -110,76 +150,131 @@ class Scraper:
                 ])
 
     def build_weather_csv(self, json, time):
+        
         f = csv.writer(open("weather.csv", "w"))
-        f.writerow(["WeatherText", "Temperature - Celcius", "timestamp"])
+        f.writerow(["WeatherText", "HasPrecip", "PrecipType", "IsDay", "TempC","FRTempC","FRTempShadeC",
+        "RelatHum","DewPointC","WindDir","WindSpeedV", "WindSpeedU","WindGustV", "WindGustU","UVIndex",
+        "UVText","VisibilityV","VisibilityU","CloudCover","CeilingV", "CeilingU","PressureV","PressureU",
+        "PressTendC", "WindChillC", "Precip1V","Precip1U","timestamp"])
         for x in json:
             f.writerow([ 
                 time,
-                x["WeatherText"], 
+                x["WeatherText"],
+                x["HasPrecipitation"],
+                x["PrecipitationType"], 
+                x["IsDayTime"],
                 x["Temperature"]['Metric']['Value'],
+                x["RealFeelTemperature"]['Metric']['Value'],
+                x["RealFeelTemperatureShade"]['Metric']['Value'],
+                x["RelativeHumidity"],
+                x["DewPoint"]['Metric']['Value'], 
+                x["Wind"]['Direction']['English'],
+                x["Wind"]["Speed"]['Metric']['Value'],
+                x["Wind"]["Speed"]['Metric']['Unit'],
+                x["WindGust"]["Speed"]['Metric']['Value'],
+                x["WindGust"]["Speed"]['Metric']['Unit'],
+                x["UVIndex"],
+                x["UVIndexText"],
+                x["Visibility"]['Metric']['Value'],
+                x["Visibility"]['Metric']['Unit'],
+                x["CloudCover"],
+                x["Ceiling"]['Metric']['Value'],
+                x["Ceiling"]['Metric']['Unit'],
+                x["Pressure"]['Metric']['Value'],
+                x["Pressure"]['Metric']['Unit'],
+                x["PressureTendency"]['Code'],
+                x["WindChillTemperature"]['Metric']['Value'],
+                x["Precip1hr"]['Metric']['Value'],
+                x["Precip1hr"]['Metric']['Unit'],
                 x["EpochTime"],
-                ])
+                ])        
 
     def drop_and_restart(self):
-        cnx = pymysql.connect(host=Scraper.SQL_HOST, user=Scraper.SQL_USER, password=Scraper.PASSWORD,
+        cnx = pymysql.connect(host=Scraper.KIERAN_RDS_HOST, user=Scraper.KIERAN_RDS_USER, password=Scraper.KIERAN_RDS_PW,
                                 database='comp30830', local_infile=1)
         cursor = cnx.cursor()
         script = """
-        drop table dynamicdata; 
+        drop table if exists dynamicdata; 
         """
         cursor.execute(script)
         script = """
         drop table weatherdata;
         """
         cursor.execute(script)
+        
         script = """
-
-                CREATE TABLE weatherdata (
+        CREATE TABLE weatherdata (
         id INT PRIMARY KEY,
-        weather_text VARCHAR(255),
-        temperature DECIMAL(3,1),
+        weather_text VARCHAR(50),
+        hasprecip VARCHAR(5),
+        preciptype VARCHAR(20),
+        isday VARCHAR(5),
+        tempc DECIMAL(7,2),
+        rftempc DECIMAL(7,2),
+        rftempshadec DECIMAL(7,2),
+        relathum INT,
+        dewpointc DECIMAL(7,2),
+        winddir VARCHAR(6),
+        windspeedv DECIMAL(7,2), 
+        windspeedu VARCHAR(6),
+        windgustv DECIMAL(7,2),
+        windgustu VARCHAR(6),
+        uvindex INT,
+        uvtext VARCHAR(15),
+        visibilityv DECIMAL(7,2),
+        visibilityu VARCHAR(6),
+        cloudCover INT(3),
+        ceilingv DECIMAL(7,2),
+        Ceilingu VARCHAR(6),
+        pressurev DECIMAL(7,2),
+        pressureu VARCHAR(6),
+        presstendc VARCHAR(1),
+        windchillc DECIMAL(7,2),
+        precipval DECIMAL(10,2),
+        precipunit VARCHAR(6),
         timestamp BIGINT
         );
         """
+
         cursor.execute(script)
         script = """
 
-CREATE TABLE dynamicdata (
-download_number INT,
-station_id INT,
-timestamp BIGINT,
-weather_id INT,
-banking VARCHAR(10),
-bonus VARCHAR(10),
-total_bike_stands INT,
-available_bike_stands INT,
-available_bikes INT,
-status VARCHAR(10),
-weekday VARCHAR(20),
-hour_and_minute INT(4),
+        CREATE TABLE dynamicdata (
+        download_number INT,
+        station_id INT,
+        timestamp BIGINT,
+        weather_id INT,
+        banking VARCHAR(10),
+        bonus VARCHAR(10),
+        total_bike_stands INT,
+        available_bike_stands INT,
+        available_bikes INT,
+        status VARCHAR(10),
+        weekday VARCHAR(20),
+        hour_and_minute INT(4),
 
-CONSTRAINT pk_station PRIMARY KEY (station_id, download_number),
+        CONSTRAINT pk_station PRIMARY KEY (station_id, download_number),
 
-CONSTRAINT station_fk
-FOREIGN KEY (station_id)
-REFERENCES staticdata (number)
-ON DELETE RESTRICT
-ON UPDATE CASCADE,
+        CONSTRAINT station_fk
+        FOREIGN KEY (station_id)
+        REFERENCES staticdata (number)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
 
-CONSTRAINT weather_fk
-FOREIGN KEY (weather_id)
-REFERENCES weatherdata (id)
-ON DELETE RESTRICT
-ON UPDATE CASCADE
-);
-     """
+        CONSTRAINT weather_fk
+        FOREIGN KEY (weather_id)
+        REFERENCES weatherdata (id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+        );
+        """
         cursor.execute(script)
         cursor.close()
         cnx.commit()
 
     def insert_to_db(self, tablename, filename):
         path = filename
-        cnx = pymysql.connect(host=Scraper.SQL_HOST, user=Scraper.SQL_USER, password=Scraper.PASSWORD,
+        cnx = pymysql.connect(host=Scraper.KIERAN_RDS_HOST, user=Scraper.KIERAN_RDS_USER, password=Scraper.KIERAN_RDS_PW,
                                 database='comp30830', local_infile=1)
         # opening the cursor    
         cursor = cnx.cursor()
---
+++ web/Private/private.py
@@ -0,0 +1,19 @@
+pw = "Claudiosanchez37"
+# stations = '/Users/raph/Sites/Learning/UCD/Semester 2/Software Engineering/group_assignment/project_directory/comp30830/databasing/stations.csv'
+# weather = '/Users/raph/Sites/Learning/UCD/Semester 2/Software Engineering/group_assignment/project_directory/comp30830/databasing/weather.csv'
+stations = "./stations.csv"
+weather = "./weather.csv"
+
+yvette_weather_api = "R5nRJsLmnyLKffAjYv6Npv0GIRASkmDu"
+raph_weather_api = "QbWKIBGQCXEEjYMp1pfeRfouHIYyWL07"
+weather_url = "http://dataservice.accuweather.com/currentconditions/v1/207931"
+jcd_api = "2029934b383854098ac75c16c62e5f48763e5934"
+
+jcd_url = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=2029934b383854098ac75c16c62e5f48763e5934"
+static_path = '/Users/raph/Sites/Learning/UCD/Semester 2/Software Engineering/group_assignment/project_directory/comp30830/databasing/static.csv'
+sql_user = 'root'
+sql_host = 'localhost'
+
+raph_rds_pw = 'arydPxG4yfWKKXz'
+raph_rds_user = 'raph'
+raph_rds_host = 'raphrds.cpspow65d2gh.us-east-1.rds.amazonaws.com'
